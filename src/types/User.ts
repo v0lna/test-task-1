4a9a3849e9a116b76265e23e23a9ed7e01dcfda7
Sergey Lebedinets <graylebedinetss@gmail.com>
2019-11-07 19:52:08 +0200

    initial commit

---
+++ src/types/User.ts
@@ -0,0 +1,15 @@
+export interface UserProfile {
+  id: string;
+  name: string;
+  sex: 'male' | 'female';
+  age: number;
+  avatart: string;
+  github_url: string;
+  username: string;
+  password: string;
+}
+// "id": "0",
+// "name": "Sergio Lenc",
+// "sex": "male",
+// "avatar": "http://www.vokrugsveta.ru/img/bx/medialibrary/967/967bb606758586d2c562713f121d6f3b.jpg",
+// "github_url": "github.com/v0lna"
---
+++ src/types/oneNews.ts
@@ -0,0 +1,7 @@
+export interface OneNews {
+  id: string;
+  createdAt: number;
+  name: string;
+  avatar: string;
+  text: string;
+}
